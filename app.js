ed951bcdbd0cc14b3fda991444a829a452a3d5a2
matthew-long-ferocia <matthew.long@ferocia.com.au>
2026-06-11 14:55:42 +1000

    ux: scroll to top on hash route changes

--- app.js
+++ app.js
@@ -156,6 +156,9 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
 
+    // Reset scroll position to top of page
+    window.scrollTo(0, 0);
+
     // Render corresponding view data
     if (target === "home") {
       isSearchDropdownsInitialized = false;
